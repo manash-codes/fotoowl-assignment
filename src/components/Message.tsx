b77d78011f5c7fd96c975a9b34ad62d17c104c35
manash-codes <manash.codes@gmail.com>
2024-12-27 17:47:27 +0530

    Add message for fresh chats

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -6,6 +6,13 @@ type MessageProps = {
 }
 
 const Message = ({ messages, recepientId }: MessageProps) => {
+
+    if (messages.length === 0) {
+        return <div className="auth-info-container">
+            <h2>Let's Chat...</h2>
+        </div>
+    }
+
     return (
         <div className="message">
             {messages.map(({ id, receiverID, text }) => (
